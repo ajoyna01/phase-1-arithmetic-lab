3eeb8e1948a6f141f83c52afaa169ec2ae8fbad7
Anna Wilson <ajoyna01@gmail.com>
2021-06-08 12:09:57 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//num1() * num2();
+//=> 62
+//var multiplyTwoNums = (num1, num2) => num1 * num2;
+//    let result = multiplyTwoNums(2, 31);
+//console.log(multiplyTwoNums(2, 31));
 
-// Write your code here
+
+//num1(31); 
+//num2(2);
+//num1 * num2;
+let num1 = 31;
+let num2 = 2;
+
+var multiply = num1 * num2;
+
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 6
+var mod = num3 % num4;
+
+var max = Math.max(1, 20);
